cb740a9dc6ec73f7e2dc8f316ef188fd0423ad98
Osanda Deshan <osanda.deshan@gmail.com>
2021-04-14 16:20:10 +0530

    Initial commit

---
+++ ExcelCSVReaderApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using ExcelCSVReaderApp.Util;
+
+namespace ExcelCSVReaderApp
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            // Set Excel and CSV file paths
+            var path = Assembly.GetCallingAssembly().CodeBase;
+            if (path == null) return;
+            var projectPath = new Uri(path.Substring(0, path.LastIndexOf("bin", StringComparison.Ordinal))).LocalPath;
+            
+            var excelFilePath = new Uri(projectPath).LocalPath + @"TestData.xlsx";
+            var csvFilePath = new Uri(projectPath).LocalPath + @"TestData.csv";
+
+            Console.WriteLine("Excel file path is: " + excelFilePath);
+            Console.WriteLine("CSV file path is: " + csvFilePath);
+
+            // Read Excel file
+            Console.WriteLine("Excel cell value is: " + new ExcelReader(excelFilePath).Read("TestDataSheet", 1, 2));
+            
+            // Read CSV file
+            Console.WriteLine("CSV cell value is: " + new CsvReader(csvFilePath).Read(1, 2));
+        }
+    }
+}
---
+++ ExcelCSVReaderApp/Util/ExcelReader.cs
@@ -0,0 +1,40 @@
+﻿using GemBox.Spreadsheet;
+
+namespace ExcelCSVReaderApp.Util
+{
+    public class ExcelReader
+    {
+        private readonly string _excelFilePath;
+
+        public ExcelReader(string excelFilePath)
+        {
+            _excelFilePath = excelFilePath;
+        }
+
+        public string Read(string sheetName, int rowIndex, int columnIndex)
+        {
+            // If using Professional version, put your serial key below
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+
+            // Load Excel workbook from file path
+            var workbook = ExcelFile.Load(_excelFilePath);
+
+            // Select the worksheet by name
+            var worksheet = workbook.Worksheets[sheetName];
+
+            // Display sheet's name
+            // Console.WriteLine("SheetName is: " + worksheet.Name);
+
+            // Select the row by index
+            var row = worksheet.Rows[rowIndex];
+
+            // Select the cell by row and column number
+            var cell = row.Cells[columnIndex];
+
+            // Display cell value
+            // Console.WriteLine("Cell value is: " + cell.Value);
+
+            return cell.Value as string;
+        }
+    }
+}
